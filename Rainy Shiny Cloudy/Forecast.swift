bfaf45d6f1e09ac589768100bb590ce9710e7b5b
AndreasSterner <andreas@sterner.io>
2017-05-03 11:05:32 +0200

    Forecast class

---
+++ Rainy Shiny Cloudy/Forecast.swift
@@ -0,0 +1,47 @@
+//
+//  Forecast.swift
+//  Rainy Shiny Cloudy
+//
+//  Created by Andreas Sterner on 2017-05-03.
+//  Copyright © 2017 Andreas Sterner. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class Forecast {
+    var _date: String!
+    var _weatherType: String!
+    var _highTemp: String!
+    var _lowTemp: String!
+    
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var highTemp: String {
+        if _highTemp == nil {
+            _highTemp = ""
+        }
+        return _highTemp
+    }
+    
+    var lowTemp: String {
+        if _lowTemp == nil {
+            _lowTemp = ""
+        }
+        return _lowTemp
+    }
+    
+}
